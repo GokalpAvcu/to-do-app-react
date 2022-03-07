a6e06668d69b990ca6993e4a3342ed5612f88b75
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-07 10:13:21 +0300

    add provider

---
+++ src/context/TodoContext.js
@@ -0,0 +1,9 @@
+import React, {createContext, useContext, useReducer} from "react";
+
+export const TodoLayerContext = useContext();
+
+export const TodoLayer = ({initialState, reducer, children}) => (
+    <TodoLayerContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </TodoLayerContext.Provider>
+)
